950639a4d48f0aca4233ff9604f20baf189cb3ac
JazmyneB <bradleyj1@email.arizona.edu>
2021-07-19 18:02:54 -0700

    added server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,19 +8,28 @@ const app = express();
 app.use(express.urlencoded({ extended : false }));
 app.use(express.json());
 
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username
+        user: 'root',
+        //MYSQL PW
+        password: 'L0vecoding!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+//Testing the connection 
+db.query(`SELECT * FROM candidates`, (err, rows) =>{
+    console.log(rows);
+});
+
 //DEfault response for any other request (not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
-
-
-
-
-
-
-
-
-
 
 
 
